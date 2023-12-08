8091c90d9fba1219be31c14d826ffd0ee1243945
KashnykovAnton <80004672+KashnykovAnton@users.noreply.github.com>
2023-12-08 15:01:23 +0100

    task 4 done.ts

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,50 @@
+class Key {
+  
+  constructor(private signature: number = Math.floor(Math.random() * 11)) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person came in.");
+    } else {
+      console.log("The door is closed.");
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is opened.");
+    } else {
+      console.log("Invalid key. The door remains closed.");
+    }
+  }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
